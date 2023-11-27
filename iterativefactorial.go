de022922dc79afdf93ec67b4b7a4b7f5ef89171b
baksoy <baran.aksoy76800@gmail.com>
2023-11-27 13:43:14 +0100

    iterativefactorial.go

--- iterativefactorial.go
+++ iterativefactorial.go
@@ -2,15 +2,19 @@ package piscine
 
 func IterativeFactorial(nb int) int {
 	result := 1
+	overflow := 1
 
-	for i := 1; i <= nb; i++ {
-		result = result * i
-	}
-	if i > 20 {
-		return 0
-	}
 	if nb < 0 {
 		return 0
 	}
+
+	for i := 0; i < nb; i++ {
+		r = r * overflow
+		overflow++
+		if i > 20 {
+			return 0
+		}
+	}
+	
 	return result
 }
