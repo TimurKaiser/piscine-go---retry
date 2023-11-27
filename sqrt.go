4245572677d993dac3594d337d4d98aa46ba0e87
baksoy <baran.aksoy76800@gmail.com>
2023-11-27 16:48:12 +0100

    sqrt.go

---
+++ sqrt.go
@@ -0,0 +1,14 @@
+package piscine
+
+func Sqrt(nb int) int {
+	result := 0
+    if nb < 0 {
+        return 0
+    }
+    for result*result <= nb {
+        if result*result == nb {
+            return result
+        }
+        result++
+    }
+}
