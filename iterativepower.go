e06da8737f20f3df11fd688fb57f9a441ffe0b0d
baksoy <baran.aksoy76800@gmail.com>
2023-11-27 14:27:17 +0100

    iterativepower.go

---
+++ iterativepower.go
@@ -0,0 +1,18 @@
+package main 
+
+func IterativePower(nb int, power int) int {
+	result := 1
+
+	if power == 0 {
+		return 1
+	}
+
+	if power < 0 {
+		return 0
+	}
+
+	for i := 0; i < power; i++ {
+		result = result * nb
+	}
+	return result
+}
