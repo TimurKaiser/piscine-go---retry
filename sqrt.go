41cd21eb9cf0fe9be791af0d0689a27e698ed2c1
baksoy <baran.aksoy76800@gmail.com>
2023-11-27 17:02:30 +0100

    sqrt.go

--- sqrt.go
+++ sqrt.go
@@ -2,10 +2,10 @@ package piscine
 
 func Sqrt(nb int) int {
 	result := 0
+	
 	if nb < 0 {
-    	return 0
+		return 0
     }
-
 	for result*result <= nb {
 		if result*result == nb {
 			return result
